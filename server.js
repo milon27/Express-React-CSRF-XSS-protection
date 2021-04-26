9847fb514bdd2e11a1f100adcc0779e5aeaa4c08
milon27 <mdjahidulislammilon@gmail.com>
2021-04-27 02:31:00 +0600

    done 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const csurf = require('csurf')
+const cors = require('cors')
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 //{
 // key: '_csrf-milon27',
 //     path: '/context-route',
@@ -17,26 +19,50 @@ app.use(cookieParser());
 //     }
 //main cookie part
 //(whenever we have a form in client- will generate a csrf token for next 10 minutes)
-const csurfMid = csurf({
-    cookie: {
-        httpOnly: true,
-        secure: false,
-        maxAge: 60//1mint // 3600 // 1-hour
-    }
-})
-app.use(csurfMid);
+// const csurfMid = csurf({
+//     cookie: {
+//         httpOnly: true,
+//         secure: false,
+//         maxAge: 60//1mint // 3600 // 1-hour
+//     }
+// })
+// app.use(csurfMid);
 
-//set a xsrf-token(body part) for all request from request csrf token. 
-app.all("*", (req, res, next) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());//we can get this on javascript Cookies.get('XSRF-TOKEN')
-    next();
-});
+// //set a xsrf-token(body part) for all request from request csrf token. 
+// app.all("*", (req, res, next) => {
+//     res.cookie("XSRF-TOKEN", req.csrfToken());//we can get this on javascript Cookies.get('XSRF-TOKEN')
+//     next();
+// });
+
+
+//demo token for post request
+const authMid = (req, res, next) => {
+    const token = req.cookies.auth
+    console.log('token-' + token);
+    if (token === 'someauthtoken') {
+        next()
+    } else {
+        res.status(400).send("unauthorized")
+    }
+}
 
 //in body : {_csrf: Cookies.get('XSRF-TOKEN')}
-app.post('/post', (req, res) => {
-    res.json({ done: req.body })
+app.post('/post', authMid, (req, res) => {
+    res.json({ name: "posted" })
 });
 
-app.get('/', (req, res) => res.send("milon"));
+app.get('/', (req, res) => {
+    res.cookie('auth', 'someauthtoken', {
+        httpOnly: true
+    })
+    res.json({ name: "milon" })
+});
 
-app.listen(3000);
+app.get('/logout', (req, res) => {
+    res.cookie('auth', '', {
+        httpOnly: true
+    })
+    res.json({ name: "milon" })
+});
+//X-XSRF-TOKEN
+app.listen(2727, () => console.log('2727 running..'));
